a9a7dc8965c6102e4d0fdc446da6a8608ff6a0d5
SERGEY <sergey.givargizov@gmail.com>
2024-07-13 14:53:18 +0400

    created film-app version 1, without loops and ststements

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 "use strict";
-window.addEventListener("DOMContentLoaded", () => {
+const countoffilms = +prompt("քանի ֆիլմ եք վերջերս դիտել");
+console.log(countoffilms);
+const DB = {
+count:countoffilms,
+movies:{},
+actors:{},
+genres:[],
+private: false
+};
+const g = prompt("որ ֆիլմն եք վերջկրս դիտել");
+const k = + prompt("ինչքան եք գնահատւմ այդ ֆիլմը");
+const l = promt("որ ֆիլմն եք վերջկրս դիտել");
+const m = + promt("ինչքան եք գնահատւմ այդ ֆիլմը");
+console.log(DB);
 
-console.log("hello dear");
-})
+DB.movies [g]=k;
+DB.movies[i]=m;
+const user = {
+    name: "sidney"
+}
+const filmname="machete"
+user.firstname ="mexico";
+user[filmname] = 21
+
+console.log(user);
